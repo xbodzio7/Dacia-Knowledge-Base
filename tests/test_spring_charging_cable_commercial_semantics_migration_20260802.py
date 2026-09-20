37c54ea818ed481c31139eca429451d0f3fc6fd5
xbodzio7 <68440306+xbodzio7@users.noreply.github.com>
2026-09-21 00:07:37 +0200

    test: align Spring historical semantics and repository counts

--- tests/test_spring_charging_cable_commercial_semantics_migration_20260802.py
+++ tests/test_spring_charging_cable_commercial_semantics_migration_20260802.py
@@ -70,7 +70,7 @@ class SpringChargingCableCommercialSemanticsMigrationTests(unittest.TestCase):
             self.assertEqual(item["currency_code"], "PLN")
             self.assertEqual(item["price_date"], "2026-08-02")
             self.assertEqual(item["source_code"], "src_pl_spring_commercial_context_20260802")
-        expression = by_configuration["spring_expression_electric70_automatic"]
+        expression = next(\n            item for item in mappings\n            if item["configuration_code"] == "spring_expression_electric70_automatic"\n            and item["source_code"] == "src_pl_spring_price_my25_stock_20260708"\n        )
         self.assertEqual(expression["availability_status"], "optional")
         self.assertEqual(expression["amount"], "1500")
         self.assertEqual(expression["currency_code"], "PLN")
